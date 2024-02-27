22c671b993dc8ea143db40e262ce9f9d2fda4250
Genadi Petkov <101999946+gpetkov1996@users.noreply.github.com>
2024-02-27 22:31:29 +0200

    Add files via upload

---
+++ Add and Subtract/Add_and_Subtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputOne = int.Parse(Console.ReadLine());
+            int inputTwo = int.Parse(Console.ReadLine());
+            int inputThree = int.Parse(Console.ReadLine());
+
+            int sum = SumOfOneAndTwo(inputOne, inputTwo);
+            int subtract = SubtractFromSum(sum, inputThree);
+            
+            Console.WriteLine($"{subtract}");
+            
+        }
+
+        private static int SubtractFromSum(int sum, int inputThree)
+        {
+            return sum - inputThree;
+        }
+
+        private static int SumOfOneAndTwo(int inputOne, int inputTwo)
+        {
+            return inputOne + inputTwo;
+        }
+    }
+}
